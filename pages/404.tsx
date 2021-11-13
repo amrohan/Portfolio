d877033aa47c19eac46ddaf81b0158f731db0a01
amrohan <amrohanx@gmail.com>
2021-11-13 12:25:24 +0530

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Link from "next/link";
 import { useEffect } from "react";
 import router from "next/dist/client/router";
 
 const NotFound = () => {
   useEffect(() => {
-    console.log("hello");
     // Setting Timout of 2s,
     setTimeout(() => {
       // using router.push to redirect user from 404 page to home page
@@ -15,10 +13,10 @@ const NotFound = () => {
   }, []);
   return (
     <div className="bg-gray-950 w-screen h-screen text-white">
-      <div className="h-full grid place-items-center">
-        <h1 className="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-pink-800">
-          404
-        </h1>
+      <div className="h-full grid place-items-center w-auto">
+        <div>
+          <img src="/404.svg" alt="" width={250} height={32} />
+        </div>
       </div>
     </div>
   );
