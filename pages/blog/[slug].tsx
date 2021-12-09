b0990548dff4cff4e16c1ef818d21d24ad9c9c26
amrohan <amrohanx@gmail.com>
2021-12-09 19:29:55 +0530

    updated all packages and import in slug.tsx

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import marked from "marked";
+import {marked} from "marked";
 import styles from "../../styles/content.module.css";
 import Link from "next/link";
 
