b6b66ab04c3814a9e9725720a9cab211c650ff58
amrohan <amrohanx@gmail.com>
2021-12-26 20:11:20 +0530

    homepage padding right fixed!

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,11 @@ export default function Home() {
       </Head>
       <div className=" h-full flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
         <div className="flex flex-col-reverse sm:flex-row items-start">
-          <div className="flex flex-col pr-6">
+          <div className="flex flex-col md:pr-8">
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-yellow-800">
               Rohan Salunkhe
             </h1>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              Developer
-            </h2>
+            <h2 className="text-gray-700 dark:text-gray-200 mb-4">Developer</h2>
             <p className="text-gray-600 dark:text-gray-400 mb-16">
               {
                 "It's an honor for me to share my passions, stories, and abilities with you. Below is a sampling of the many things I've accomplished. "
@@ -38,10 +36,10 @@ export default function Home() {
           </div>
         </div>
         <h3 className="font-bold text-2xl md:text34xl tracking-tight mb-6 text-black dark:text-white underline decoration-indigo-500">
-          Featured{" "} 
+          Featured{" "}
           <span className="underline decoration-pink-500">Projects</span>
         </h3>
-        <section className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8 place-content-center group ">
+        <section className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
           <BlogPostCard
             title="A Telegram Bot"
             slug="telegram-chat-bot"
