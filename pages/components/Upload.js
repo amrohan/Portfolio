c144c8ee75994fb80f16a6c4cd279efd586ad20d
amrohan <amrohanx@gmail.com>
2021-12-26 11:14:15 +0530

    small bug fixed

--- pages/components/Upload.js
+++ pages/components/Upload.js
@@ -101,7 +101,9 @@ function Upload() {
                     {JSON.stringify(uploadData, null, 2)}
                   </pre> */}
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                    <a href={uploadData} className="dark:text-white">Check Image</a>
+                    <a href={uploadData} className="dark:text-white">
+                      Check Image
+                    </a>
                   </button>
                 </code>
               </div>
