d11d5edc7c322fdc5ef127e5d53a5e6a764773d9
amrohan <amrohanx@gmail.com>
2021-12-26 20:49:58 +0530

    Comlete Website @V2.1

--- pages/components/MarkdownWrapper.tsx
+++ pages/components/MarkdownWrapper.tsx
@@ -4,7 +4,7 @@ export default function MarkdownWrapper({ children }) {
   // Creating this commponent to wrap the markdown content with mention classes
   // Passing Children attribute to the component so it can render the give child into the component
   return (
-    <div className="mt-5 prose prose-slate prose-img:shadow-2xl prose-strong:text-yellow-400 prose-img:w-6/12 prose-img:mx-auto prose-video:mx:auto prose-blockquote:text-cyan-600 lg:prose-l dark:prose-invert prose-a:text-teal-600 hover:prose-a:text-teal-500 prose-img:rounded-2xl">
+    <div className="mt-5 prose prose-slate prose-img:shadow-2xl prose-video:rounded-2xl  prose-strong:text-yellow-400 prose-img:w-6/12 prose-img:mx-auto prose-video:mx-auto prose-blockquote:text-cyan-600 lg:prose-l dark:prose-invert prose-a:text-teal-600 hover:prose-a:text-teal-500 prose-img:rounded-2xl">
       {children}
     </div>
   );
