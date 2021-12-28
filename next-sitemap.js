791cba9582b5c344a57bef54d283f72036aa0c33
amrohan <amrohanx@gmail.com>
2021-12-28 13:39:01 +0530

    complete website @v2.1

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://rohan.ml",
+  siteUrl: process.env.SITE_URL || "https://amrohan.xyz",
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
