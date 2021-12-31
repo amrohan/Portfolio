c2a5b5e43badc6d15cf502a5eaf9b7a872010e3c
amrohan <amrohanx@gmail.com>
2021-12-31 21:28:51 +0530

    website v@3

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -6,8 +6,6 @@ export default function MetaTags({title,description,image}) {
         <div>
             <Head>
                 <title>{title}</title>
-                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="description" content={description} />
 
