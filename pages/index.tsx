b5cff44e2852637e13684b87e0e24448a0f9a271
amrohan <amrohanx@gmail.com>
2022-01-03 12:49:56 +0530

    updated homepage and navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,77 @@
 import MetaTags from "../components/MetaTags";
-import BlogPostCard from "../components/BlogPostCard";
 import Link from "next/link";
 
 export default function Home() {
   var homepage = "Homepage";
   return (
-    <div className="p-8 w-full h-full lg:h-screen pt-28 md:pt-36 dark:bg-gray-950 dark:text-white overflow-hidden">
-      {/* Adding Meta tags Here */}
+    <div className="bg-white dark:bg-gray-950 flex relative z-20 items-center overflow-hidden h-screen">
       <MetaTags
         title="Rohan Salunkhe - amrohan personal website"
         description="Rohan Salunkhe's personal website, which includes links to my social media accounts as well as links to my most recent projects, blogs, and posts on GitHub, dev.to, and hashnode."
         image="https://res.cloudinary.com/amrohan/image/upload/v1641026544/Images/e1ffa8bll07ebpal4esa.png"
       />
-      <div className=" h-full flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
-        <div className="flex flex-col-reverse sm:flex-row items-start">
-          <div className="flex flex-col md:pr-8">
-            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 ">
-              Rohan Salunkhe
-            </h1>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">Developer</h2>
-            <p className="text-gray-600 dark:text-gray-400 mb-16">
-              {
-                "It's an honor for me to share my passions, stories, and abilities with you. Below is a sampling of the many things I've accomplished. "
-              }
-            </p>
-          </div>
-          <div className="w-[80px] sm:w-[176px] mb-8 sm:mb-0 mr-auto">
-            <img
-              alt="Rohan Salunkhe"
-              height={176}
-              width={176}
-              src="/avatar.jpg"
-              className="rounded-full filter grayscale"
-            />
+      <div className="container mx-auto px-6 flex relative py-16">
+        <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20 lg:ml-14 ">
+          <h1 className="sm:pt-12 text-5xl mb-4 sm:text-6xl flex flex-col leading-none dark:text-white text-gray-800">
+            Rohan Salunkhe
+            <span className="text-xl">Developer</span>
+          </h1>
+          <p className="text-sm sm:text-base text-gray-700 dark:text-white">
+            {
+              "It's an honor for me to share my passions, stories, and abilities with you. Below is a sampling of the many things I've accomplished."
+            }
+          </p>
+          <div className="flex mt-8">
+            <Link href="/blog">
+              <a className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white text-md">
+                Read Blog
+              </a>
+            </Link>
+            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-purple-300 ">
+              <a href="https://github.com/amrohan" className="px-2">
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </li>
+
+            {/* Instagram */}
+            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-pink-300">
+              <a href="https://www.instagram.com/amrohann" className="px-2">
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M15.233 5.488c-.843-.038-1.097-.046-3.233-.046s-2.389.008-3.232.046c-2.17.099-3.181 1.127-3.279 3.279-.039.844-.048 1.097-.048 3.233s.009 2.389.047 3.233c.099 2.148 1.106 3.18 3.279 3.279.843.038 1.097.047 3.233.047 2.137 0 2.39-.008 3.233-.046 2.17-.099 3.18-1.129 3.279-3.279.038-.844.046-1.097.046-3.233s-.008-2.389-.046-3.232c-.099-2.153-1.111-3.182-3.279-3.281zm-3.233 10.62c-2.269 0-4.108-1.839-4.108-4.108 0-2.269 1.84-4.108 4.108-4.108s4.108 1.839 4.108 4.108c0 2.269-1.839 4.108-4.108 4.108zm4.271-7.418c-.53 0-.96-.43-.96-.96s.43-.96.96-.96.96.43.96.96-.43.96-.96.96zm-1.604 3.31c0 1.473-1.194 2.667-2.667 2.667s-2.667-1.194-2.667-2.667c0-1.473 1.194-2.667 2.667-2.667s2.667 1.194 2.667 2.667zm4.333-12h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm.952 15.298c-.132 2.909-1.751 4.521-4.653 4.654-.854.039-1.126.048-3.299.048s-2.444-.009-3.298-.048c-2.908-.133-4.52-1.748-4.654-4.654-.039-.853-.048-1.125-.048-3.298 0-2.172.009-2.445.048-3.298.134-2.908 1.748-4.521 4.654-4.653.854-.04 1.125-.049 3.298-.049s2.445.009 3.299.048c2.908.133 4.523 1.751 4.653 4.653.039.854.048 1.127.048 3.299 0 2.173-.009 2.445-.048 3.298z" />
+                </svg>
+              </a>
+            </li>
+
+            {/* Twitter */}
+            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-blue-400">
+              <a href="https://www.twitter.com/rohansalunkhe_" className="px-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                </svg>
+              </a>
+            </li>
           </div>
         </div>
-        <h3 className="font-bold text-2xl md:text34xl tracking-tight mb-6 text-black dark:text-white underline decoration-indigo-500">
-          Featured{" "}
-          <span className="underline decoration-pink-500">Projects</span>
-        </h3>
-        <section className="mt-4 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
-          <BlogPostCard title="A Telegram Bot" slug="telegram-chat-bot" />
-          <BlogPostCard
-            title="A Link Tree Porject"
-            slug="a-linktree-alternative"
-          />
-        </section>
-        <Link href="/blog">
-          <a className=" h-7 flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all">
-            Read all posts
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="h-8 w-6 ml-2 text-pink-500"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-              />
-            </svg>
-          </a>
-        </Link>
       </div>
     </div>
   );
