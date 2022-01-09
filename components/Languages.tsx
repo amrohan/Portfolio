609f68dda1136238a3646b9536b8fdf89a916540
amrohan <amrohanx@gmail.com>
2022-01-09 20:22:47 +0530

    updated design && languages added

---
+++ components/Languages.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+export default function Languages() {
+  return (
+    <section className="text-gray-400 bg-gray-950">
+      <h1 className="text-center dark:text-white text-2xl font-bold underline decoration-pink-500">
+        Languages I Know
+      </h1>
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="flex flex-wrap w-full">
+          <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+              </div>
+              <div className="flex-shrink-0 w-10 h-10 rounded-full  inline-flex items-center justify-center text-white relative z-10">
+                <img src="/python.png" alt="" />
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                  Python
+                </h2>
+                <p className="leading-relaxed">
+                  I have python and{" "}
+                  <span className="underline decoration-indigo-400">
+                    MongoDb
+                  </span>{" "}
+                  manager to construct a python chatbot
+                </p>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+              </div>
+              <div className="flex-shrink-0 w-10 h-10 rounded-full  inline-flex items-center justify-center text-white relative z-10">
+                <img src="/java.png" alt="" />
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                  Java
+                </h2>
+                <p className="leading-relaxed">
+                  Using java and android studio, I created an android
+                  application.
+                </p>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+              </div>
+              <div className="flex-shrink-0 w-10 h-10 rounded-full inline-flex items-center justify-center text-white relative z-10">
+                <img src="/c.png" alt="C-Programming lang image" />
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                  C
+                </h2>
+                <p className="leading-relaxed">
+                  Used c to create a simple calculator.
+                </p>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+              </div>
+              <div className="flex-shrink-0 w-10 h-10 rounded-full inline-flex items-center justify-center text-white relative z-10">
+                <img src="/cplus.png" alt="c ++ image" />
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                  C++
+                </h2>
+                <p className="leading-relaxed">
+                  Used c++ to create a simple terminal game.
+                </p>
+              </div>
+            </div>
+            <div className="flex relative">
+              <div className="flex-shrink-0 w-10 h-10 rounded-full inline-flex items-center justify-center text-white relative z-10">
+                <img src="/ar.webp" alt="arrow" />
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-lg text-white mb-1 tracking-wider">
+                  Frameworks
+                </h2>
+                <p className="leading-relaxed">
+                  {
+                    "React and vue are frameworks that I am familiar with. Working with Next js, on the other hand, is a pleasure for me. I used next js to make my portfolio website."
+                  }
+                </p>
+              </div>
+            </div>
+          </div>
+          <img
+            className="lg:w-3/5 md:w-1/2 object-contain object-center rounded-lg md:mt-0 mt-12"
+            src="/code.gif"
+            alt="step"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,52 @@
 import MetaTags from "../components/MetaTags";
 import Link from "next/link";
+import Languages from "../components/Languages";
 
 export default function Home() {
-  var homepage = "Homepage";
   return (
-    <div className="bg-white dark:bg-gray-950 flex relative z-20 items-center overflow-hidden h-screen">
+    <>
       <MetaTags
         title="Rohan Salunkhe - amrohan personal website"
         description="Rohan Salunkhe's personal website, which includes links to my social media accounts as well as links to my most recent projects, blogs, and posts on GitHub, dev.to, and hashnode."
         image="https://res.cloudinary.com/amrohan/image/upload/v1641026544/Images/e1ffa8bll07ebpal4esa.png"
       />
-      <div className="container mx-auto px-6 flex relative py-16">
-        <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20 lg:ml-14 ">
-          <h1 className="sm:pt-12 text-5xl mb-4 sm:text-6xl flex flex-col leading-none dark:text-white text-gray-800">
-            Rohan Salunkhe
-            <span className="text-xl">Developer</span>
-          </h1>
-          <p className="text-sm sm:text-base text-gray-700 dark:text-white">
-            {
-              "It's an honor for me to share my passions, stories, and abilities with you. Below is a sampling of the many things I've accomplished."
-            }
-          </p>
-          <div className="flex mt-8">
-            <Link href="/blog">
-              <a className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-black dark:text-white hover:bg-pink-500 hover:text-white text-md">
-                Read Blog
-              </a>
-            </Link>
-            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-purple-300 ">
-              <a href="https://github.com/amrohan" className="px-2">
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-              </a>
-            </li>
+      <section className="text-gray-400 bg-gray-950 overflow-hidden">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+            <img
+              className=" mx-auto object-center rounded w-3/6 md:w-9/12"
+              alt="hero"
+              src="/hero.svg"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              Rohan Salunkhe
+              <br className="lg:inline-block" />
+              <span className="text-[20px]">A Developer</span>
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              {
+                "It's an honor for me to share my passions, stories, and abilities with you. Below is a sampling of the many things I've accomplished."
+              }
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white border-pink-600 border-2 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg">
+                <Link href="/blog">
+                  <a aria-label="Right Align">Blog</a>
+                </Link>
+              </button>
 
-            {/* Instagram */}
-            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-pink-300">
-              <a href="https://www.instagram.com/amrohann" className="px-2">
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M15.233 5.488c-.843-.038-1.097-.046-3.233-.046s-2.389.008-3.232.046c-2.17.099-3.181 1.127-3.279 3.279-.039.844-.048 1.097-.048 3.233s.009 2.389.047 3.233c.099 2.148 1.106 3.18 3.279 3.279.843.038 1.097.047 3.233.047 2.137 0 2.39-.008 3.233-.046 2.17-.099 3.18-1.129 3.279-3.279.038-.844.046-1.097.046-3.233s-.008-2.389-.046-3.232c-.099-2.153-1.111-3.182-3.279-3.281zm-3.233 10.62c-2.269 0-4.108-1.839-4.108-4.108 0-2.269 1.84-4.108 4.108-4.108s4.108 1.839 4.108 4.108c0 2.269-1.839 4.108-4.108 4.108zm4.271-7.418c-.53 0-.96-.43-.96-.96s.43-.96.96-.96.96.43.96.96-.43.96-.96.96zm-1.604 3.31c0 1.473-1.194 2.667-2.667 2.667s-2.667-1.194-2.667-2.667c0-1.473 1.194-2.667 2.667-2.667s2.667 1.194 2.667 2.667zm4.333-12h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm.952 15.298c-.132 2.909-1.751 4.521-4.653 4.654-.854.039-1.126.048-3.299.048s-2.444-.009-3.298-.048c-2.908-.133-4.52-1.748-4.654-4.654-.039-.853-.048-1.125-.048-3.298 0-2.172.009-2.445.048-3.298.134-2.908 1.748-4.521 4.654-4.653.854-.04 1.125-.049 3.298-.049s2.445.009 3.299.048c2.908.133 4.523 1.751 4.653 4.653.039.854.048 1.127.048 3.299 0 2.173-.009 2.445-.048 3.298z" />
-                </svg>
-              </a>
-            </li>
-
-            {/* Twitter */}
-            <li className="hidden sm:flex h-full pt-3 dark:text-white hover:text-blue-400">
-              <a href="https://www.twitter.com/rohansalunkhe_" className="px-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                </svg>
-              </a>
-            </li>
+              {/* <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                Button
+              </button> */}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+        <Languages />
+      </section>
+    </>
   );
 }
