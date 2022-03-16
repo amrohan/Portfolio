a23c84644ad2aa947d6ac2721aa2dff4139b5454
amrohan <amrohanx@gmail.com>
2022-03-16 12:03:45 +0530

    added analytics && updated packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
 import "../styles/index.css";
 import Layout from "../components/Layout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-1KZ1ECS9XR", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en"> 
-      <Head />
+    <Html lang="en">
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-123456789"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1KZ1ECS9XR', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
